2337a4d4b4db3ec2ff111c95d57c5f96a7e23e55
Ivan <RogerpIvan@yandex.ru>
2023-12-26 18:13:58 +0300

    Исправления

---
+++ script.js
@@ -0,0 +1,43 @@
+function getNumbers(){
+    const numbers = {}
+    const inputText = document.querySelector('input').value
+    let numbersArr = inputText.split(' ').filter(number => !isNaN(Number(number)) && number !== '')
+
+    for(let number of numbersArr){
+        if(!numbers.hasOwnProperty(number)){
+            numbers[number] = 1
+        }
+        else {
+            numbers[number]++
+        }
+    }
+    return numbers
+}
+
+
+function drawDiagram(){
+    const MAX_COLUMN_HEIGHT = 650
+    const numbers = getNumbers()
+    let columnElement = document.getElementsByClassName('diagram-column')[0]
+    const maxNumber = Math.max(...Object.values(numbers))
+
+    clearDiagram()
+
+    for(let number of Object.keys(numbers)) {
+        const newColumnElement = columnElement.cloneNode(true)
+        newColumnElement.style.display = 'flex'
+
+        newColumnElement.style.height = `${(MAX_COLUMN_HEIGHT*numbers[number])/maxNumber}px`
+        newColumnElement.childNodes[0].textContent = number
+        columnElement.after(newColumnElement)
+        columnElement = newColumnElement
+    }
+}
+
+
+function clearDiagram(){
+    const columns = Array.from(document.getElementsByClassName('diagram-column'))
+    for(let i =1;i<columns.length;i++){
+        columns[i].remove()
+    }
+}
