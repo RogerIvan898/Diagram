b63a25d47656da949054b084ff7c9c5081418101
Ivan <RogerpIvan@yandex.ru>
2024-01-29 10:20:16 +0300

    promise race is moved to a function

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const diagramElement = document.getElementById('diagram')
 let columns = []
 let isSorting = false
 let cancelSorting = () => {}
+let currentSorting = null
 
 buttonSortToMin.addEventListener('click', () => sortColumns(sortMin))
 buttonSortToMax.addEventListener('click', () => sortColumns(sortMax))
@@ -23,8 +24,6 @@ function getNumbers(){
 async function createDiagram(){
   const numbers = getNumbers()
   if(numbers.length){
-    isSorting = false
-    
     cancelSorting()
     clearDiagram()
 
@@ -64,57 +63,62 @@ function createElement(tagName, ...classes){
 
 async function sortColumns(compareFunction){
   cancelSorting()
-
-  let cancelPromise = new Promise(cancel => cancelSorting = () => {
-    isSorting = false
-    cancel()
-  })
-
-  let firstColumn = columns[0]
-  let secondColumn = columns[1]
-
-  addCompareStyle(firstColumn, secondColumn)
-  await delay(400)
-
   isSorting = true
 
   for(let i = 0; i < columns.length; i++) {
     for (let j = 0; j < columns.length - i - 1; j++) {
-      firstColumn = columns[j]
-      secondColumn = columns[j + 1]
+      let firstColumn = columns[j]
+      let secondColumn = columns[j + 1]
+      let swap = null
 
-      const result = await Promise.race([
-        cancelPromise,
+      await Promise.all([
+        animateColumnSwap(firstColumn, secondColumn, compareFunction),
         new Promise(async (resolve) => {
           addCompareStyle(firstColumn, secondColumn)
           await delay(800)
-
-          resolve(async () => {
-            const compareResult = compareFunction(firstColumn.textContent, secondColumn.textContent)
-            if (compareResult > 0 && isSorting) {
-              firstColumn.style.transform = 'translateX(calc(100% + 10px))'
-              secondColumn.style.transform = 'translateX(calc(-100% - 10px))'
-
-              await delay(800)
-              if(isSorting) {
-                swapColumns(j, j + 1)
-              }
-                firstColumn.style.transform = 'translateX(0)'
-                secondColumn.style.transform = 'translateX(0)'
-            }
-          })
+          resolve()
         })
-      ])
+      ]).then(results => swap = results[0])
 
-      result && await result()
-
+      swap && await swap()
       removeCompareStyle(firstColumn, secondColumn)
 
-      if(!result){
+      if(!isSorting || !swap){
         return
       }
     }
   }
+}
+
+function animateColumnSwap(firstColumn, secondColumn, compareFunction){
+  const cancelPromise = new Promise(resolve => cancelSorting = () => {
+    isSorting = false
+    resolve()
+  })
+
+  return Promise.race([
+    cancelPromise,
+    new Promise(async (resolve) => {
+      await delay(800)
+
+      resolve(async () => {
+          const compareResult = compareFunction(+firstColumn.textContent, +secondColumn.textContent)
+          if (compareResult > 0 && isSorting) {
+            firstColumn.style.transform = 'translateX(calc(100% + 10px))'
+            secondColumn.style.transform = 'translateX(calc((-100% - 10px))'
+
+            await delay(800)
+
+            if (isSorting) {
+              swapColumns(firstColumn, secondColumn)
+            }
+
+            firstColumn.style.transform = 'translateX(0)'
+            secondColumn.style.transform = 'translateX(0)'
+          }
+        })
+    })
+  ])
 }
 
 function addCompareStyle(...columns){
@@ -125,9 +129,9 @@ function removeCompareStyle(...columns){
   columns.forEach(column => column.classList.remove('column-compare'))
 }
 
-async function swapColumns(firstColumnIndex, secondColumnIndex) {
-  const firstColumn = columns[firstColumnIndex]
-  const secondColumn = columns[secondColumnIndex]
+async function swapColumns(firstColumn, secondColumn) {
+  const firstColumnIndex = columns.indexOf(firstColumn)
+  const secondColumnIndex = columns.indexOf(secondColumn)
 
   diagramElement.insertBefore(firstColumn, secondColumn)
   diagramElement.insertBefore(secondColumn, firstColumn)
